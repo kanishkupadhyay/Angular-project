e49613978769deb7ee84c44cdb54d007eb3337db
Shivam Upadhayay <shivam.upadhyay@codinova.com>
2021-02-12 11:39:57 +0530

    make it better

--- db/index.js
+++ db/index.js
@@ -2,9 +2,9 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('data.json')
 const middlewares = jsonServer.defaults()
- 
+
 server.use(middlewares)
 server.use(router)
-server.listen(process.env.PORT|| 3000, () => {
-  console.log('JSON Server is running')
+server.listen(process.env.PORT || 3000, () => {
+    console.log('JSON Server is running')
 })
